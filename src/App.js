997969706fd76c35d2d4bf420f378724c33a88b7
luislaurel <luislaurel133@gmail.com>
2022-08-22 12:15:35 -0600

    Add on click to squares

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+    const handleGamePlay = (index) => {
+       let newSquares = [...squares]
+        newSquares[index] = "X"
+       setSquares(newSquares)
+console.log(newSquares[index])
+    }
+
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className="board">
-        {squares.map((value) => {
-          return <Square/>
+        {squares.map((value, index) => {
+          return <Square value={value} index={index} handleGamePlay={handleGamePlay}/>
         })}
       </div>
     </>
